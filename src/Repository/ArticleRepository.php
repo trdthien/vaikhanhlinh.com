842f5082163f6c212c64ca65d425284bd5bbdeec
Trinh Duc Thien <td.thien@shopmacher.de>
2018-06-02 14:18:08 +0700

    add article repository

---
+++ src/Repository/ArticleRepository.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Article;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * ArticleRepository.
+ */
+class ArticleRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Article::class);
+    }
+}
